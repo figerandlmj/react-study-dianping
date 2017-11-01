ccc7560c5253b8437421b9c7b8d5dc4550772e02
figerandlmj <2270200373@qq.com>
2017-11-01 10:27:32 +0800

    update webpack.production.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,7 +21,7 @@ module.exports = {
   output: {
     path: __dirname + "/build",
     filename: "[name].[chunkhash:8].js",
-    publicPath: '/'
+    // publicPath: '/'
   },
 
   resolve:{
@@ -34,7 +34,7 @@ module.exports = {
         { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss!less') },
         { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss') },
         { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/[name].[chunkhash:8].[ext]' },
-        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[chunkhash:8].[ext]'}
+        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[ext]'}
     ]
   },
   postcss: [
